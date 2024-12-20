d63c47f63ff38d266cf39868ca7c551e2bc5838d
anjaliyadav-10042006 <anjcse9119@gmail.com>
2024-12-20 17:18:09 +0530

    Add files via upload

---
+++ AI/bhagvad.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+#load excel file
+file_path='bhagavad-gita.xlsx' 
+df=pd.read_excel(file_path)
+
+#convert each row to alist
+chunks = df.values.tolist()
+
+#file to save the output
+output_file_path = 'output.txt'
+
+#write each chunk to the separator on text file
+with open(output_file_path,'w') as file:
+    for chunk in chunks:
+        #convert list to string & add separator
+        file.write(str(chunk) + "\n\n")
+        
+print(f" Data written to {output_file_path}")        
+        
